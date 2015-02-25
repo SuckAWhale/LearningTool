66493a624acbdcc7c59c17e86698910e0b2141ae
SuckAWhale <turtlcraft@hotmail.com>
2015-02-24 19:11:59 -0700

    Renamed form1 to main

--- LearningTool/Main.Designer.cs
+++ LearningTool/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LearningTool
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
--- LearningTool/Program.cs
+++ LearningTool/Program.cs
@@ -16,7 +16,7 @@ namespace LearningTool
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
